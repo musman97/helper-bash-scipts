d64d52f442362a9c2d604d405c0ef4c87cdef07d
musman97 <zahoor.usman.m@gmail.com>
2024-05-08 09:34:04 +0500

    fix: Use correct search string

--- setup-scripts.sh
+++ setup-scripts.sh
@@ -17,10 +17,11 @@ chmod -R u+x "$INSTALL_DIR"
 SHELL_CONFIG_FILE="$HOME/.zshrc"
 
 # Add installation directory to shell config file if not already present
-if [ -n "$SHELL_CONFIG_FILE" ] && ! grep -qF "export PATH=\"$INSTALL_DIR:" "$SHELL_CONFIG_FILE"; then
-    echo "\n#Add helper scripts to PATH\n" >> "$SHELL_CONFIG_FILE"
-    echo "export PATH=\"$INSTALL_DIR:\$PATH\"" >> "$SHELL_CONFIG_FILE"
+if [ -n "$SHELL_CONFIG_FILE" ] && ! grep -qF "export PATH=\"\$PATH:\$HOME/scripts\"" "$SHELL_CONFIG_FILE"; then
+    echo "\n# Add helper scripts to PATH\n" >> "$SHELL_CONFIG_FILE"
+    echo "export PATH=\"\$PATH:\$HOME/scripts\"" >> "$SHELL_CONFIG_FILE"
     source "$SHELL_CONFIG_FILE"  # Update current shell with new PATH
+    echo "Helper scripts added to PATH"
 else
     echo "Scripts path already added in zshrc"
 fi
